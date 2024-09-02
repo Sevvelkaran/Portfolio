acdc5a41d060207ef562632e5c22ea4bb3900eaa
Sevvelkaran <2k21it50@kiot.ac.in>
2024-09-02 17:32:46 +0530

    first commit

---
+++ src/constans/index.js
@@ -0,0 +1,208 @@
+import {
+    mobile,
+    backend,
+    creator,
+    web,
+    javascript,
+    typescript,
+    html,
+    css,
+    reactjs,
+    redux,
+    tailwind,
+    nodejs,
+    mongodb,
+    git,
+    figma,
+    docker,
+    meta,
+    graspear,
+    tesla,
+    shopify,
+    carrent,
+    jobit,
+    tripguide,
+    threejs,
+  } from "../assets";
+  
+  export const navLinks = [
+    {
+      id: "about",
+      title: "About",
+    },
+    {
+      id: "work",
+      title: "Work",
+    },
+    {
+      id: "contact",
+      title: "Contact",
+    },
+  ];
+  
+  const services = [
+    {
+      title: "Web Development",
+      icon: web,
+    },
+    {
+      title: "Software Development",
+      icon: mobile,
+    },
+    {
+      title: "Application Development",
+      icon: backend,
+    },
+    {
+      title: "AGILE Methodology",
+      icon: creator,
+    },
+  ];
+  
+  const technologies = [
+    {
+      name: "HTML 5",
+      icon: html,
+    },
+    {
+      name: "CSS 3",
+      icon: css,
+    },
+    {
+      name: "JavaScript",
+      icon: javascript,
+    },
+    {
+      name: "TypeScript",
+      icon: typescript,
+    },
+    {
+      name: "React JS",
+      icon: reactjs,
+    },
+    
+    {
+      name: "Tailwind CSS",
+      icon: tailwind,
+    },
+    {
+      name: "MongoDB",
+      icon: mongodb,
+    },
+    {
+      name: "git",
+      icon: git,
+    },
+    {
+      name: "figma",
+      icon: figma,
+    },
+    
+  ];
+  
+  const experiences = [
+    {
+      title: "Software Developer",
+      company_name: "Graspears",
+      icon: graspear,
+      iconBg: "#383E56",
+      date: "January 2023 - January 2024",
+      points: [
+        "Worked part-time as a Software Developer.",
+        "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+        "Implementing responsive design and ensuring cross-browser compatibility.",
+        "Participating in code reviews and providing constructive feedback to other developers.",
+      ],
+    },
+    
+  ];
+  
+  const testimonials = [
+    {
+      testimonial:
+        "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+      name: "Sara Lee",
+      designation: "CFO",
+      company: "Acme Co",
+      image: "https://randomuser.me/api/portraits/women/4.jpg",
+    },
+    {
+      testimonial:
+        "I've never met a web developer who truly cares about their clients' success like Rick does.",
+      name: "Chris Brown",
+      designation: "COO",
+      company: "DEF Corp",
+      image: "https://randomuser.me/api/portraits/men/5.jpg",
+    },
+    {
+      testimonial:
+        "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+      name: "Lisa Wang",
+      designation: "CTO",
+      company: "456 Enterprises",
+      image: "https://randomuser.me/api/portraits/women/6.jpg",
+    },
+  ];
+  
+  const projects = [
+    {
+      name: "Web Scraping",
+      description:
+        "web scraping application enables users to extract valuable data from websites quickly and efficiently. It provides a user-friendly interface for configuring scraping tasks and visualizing the collected data.",
+      tags: [
+        {
+          name: "react",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "mongodb",
+          color: "green-text-gradient",
+        },
+        {
+          name: "tailwind",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: carrent,
+      source_code_link: "https://github.com/Sevvelkaran/MarketMuse",
+    },
+    {
+      name: "Graship",
+      description:
+        "The ultimate web scraping solution designed to effortlessly gather and manage web data. Our powerful tool simplifies data extraction, enabling you to focus on insights rather than the complexities of data collection.",
+      tags: [
+        {
+          name: "HTML",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "CSS",
+          color: "green-text-gradient",
+        },
+        {
+          name: "JS",
+          color: "pink-text-gradient",
+        },
+       
+      ],
+      image: jobit,
+      source_code_link: "https://github.com/",
+    },
+    {
+      name: "L&T Blaster Tunnel",
+      description:
+        "With the use of Burp Suite a leading web security testing tool. This powerful combination enhances your data extraction capabilities while ensuring robust security and vulnerability assessment.",
+      tags: [
+        {
+          name: "Burp Suite",
+          color: "blue-text-gradient",
+        },
+       
+      ],
+      image: tripguide,
+      source_code_link: "https://github.com/",
+    },
+    
+  ];
+  
+  export { services, technologies, experiences, testimonials, projects };
